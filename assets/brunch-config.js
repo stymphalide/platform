967aa8f579b5415334529109a973a0b1cfd062cd
stymphalide <angelo.birrer@gmail.com>
2017-12-26 22:05:26 +0100

    Modified the brunch-config file to automatically compile the elm-code into the js directory. P.110

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -37,7 +37,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ["static", "css", "js", "vendor", "elm"],
     // Where to compile files to
     public: "../priv/static"
   },
@@ -47,7 +47,12 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
-    }
+    },
+    elmBrunch: {
+      mainModules: ["elm/Main.elm"],
+      makeParameters: ["--debug"],
+      outputFolder: "../assets/js"
+  }
   },
 
   modules: {
