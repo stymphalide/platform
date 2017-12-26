30fa91e18ef200b9af426d781a1ecc9186dd0d77
stymphalide <angelo.birrer@gmail.com>
2017-12-26 22:26:26 +0100

    Added a little view.

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,9 +1,29 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (class)
 
 
-
+-- MAIN
 main : Html msg
 main =
-    text "Hello from Elm inside Phoenix!"
+    div []
+        [ h1 [] [text "Games"]
+        , gamesIndex
+        ]
+
+
+gamesIndex : Html msg
+gamesIndex =
+    div [class "games-index"] [gamesList]
+
+gamesList : Html msg
+gamesList =
+    ul [class "games-list"] 
+        [ gamesListItem "Platform Game"
+        , gamesListItem "Adventure Game"
+        ]
+
+gamesListItem : String -> Html msg
+gamesListItem game =
+    li [] [text game]
