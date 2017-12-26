1693283b0c8495edee9eda173c49a66c0a34ae5d
stymphalide <angelo.birrer@gmail.com>
2017-12-26 18:16:42 +0100

    Added a registration changeset function and a put_pass_digest function.

--- lib/platform/accounts/player.ex
+++ lib/platform/accounts/player.ex
@@ -23,4 +23,25 @@ defmodule Platform.Accounts.Player do
     |> validate_length(:password, min: 6, max: 100)
     |> put_pass_digest()
   end
+
+  @doc false
+  def registration_changeset(%Player{} = player, attrs) do
+    player
+    |> cast(attrs, [:password, :username])
+    |> validate_required([:password, :username])
+    |> validate_length(:username, min: 2, max: 100)
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_digest()
+  end
+
+  defp put_pass_digest(changeset) do
+    case changeset do
+      %Ecto.Changeset(valid?: true, changes: %{password: pass}) ->
+        put_change(changeset, :password_digest, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+        
+    end
+  end
+
 end
