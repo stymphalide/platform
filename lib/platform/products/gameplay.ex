df9efb35b5cd7fe91b43b4dcf62d9c7eb1c0985b
stymphalide <angelo.birrer@gmail.com>
2017-12-26 21:06:31 +0100

    Added a new schema for the gameplays, that belongs to both table and players. p.90

---
+++ lib/platform/products/gameplay.ex
@@ -0,0 +1,23 @@
+defmodule Platform.Products.Gameplay do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Platform.Products.Gameplay
+  alias Platform.Products.Game
+  alias Platform.Accounts.Player
+
+
+  schema "gameplays" do
+    belongs_to :game, Game
+    belongs_to :player, Player
+
+    field :player_score, :integer, default: 0
+  end
+
+  @doc false
+  def changeset(%Gameplay{} = gameplay, attrs) do
+    gameplay
+    |> cast(attrs, [:game, :player, :player_score])
+    |> validate_required([:game, :player, :player_score])
+  end
+
+end
