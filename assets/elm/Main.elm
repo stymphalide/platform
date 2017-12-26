fa12dde6ac9bd2ff892d16f01c08c1806908c85e
stymphalide <angelo.birrer@gmail.com>
2017-12-27 00:39:33 +0100

    Added a feature section.

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
 import Http
 import Json.Decode as Decode
@@ -137,8 +137,28 @@ view model =
 
 featured : Model -> Html Msg
 featured model =
-    div [ class "row featured" ]
-        [ h1 [] [text "Featured"] ]
+    case featuredGame model.gamesList of
+        Just game ->
+            div [ class "row featured" ]
+                [ div [ class "container" ]
+                    [ div [ class "featured-img" ]
+                        [ img [ class "featured-thumbnail", src game.thumbnail] [] ]
+                    , div [ class "featured-data" ]
+                        [ h1 [] [ text "Featured" ]
+                        , h2 [] [ text game.title ]
+                        , p [] [ text game.description ]
+                        , button [ class "btn btn-lg btn-primary" ] [ text "Play Now!" ]
+                        ]
+                    ]
+                ]
+        Nothing ->
+            div [] []
+featuredGame : List Game -> Maybe Game
+featuredGame games = 
+    games
+        |> List.filter .featured
+        |> List.head
+
 
 gamesIndex : Model -> Html Msg
 gamesIndex model =
