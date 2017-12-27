a4ad5f74faae75c40f7855470d7644781a8b30a6
stymphalide <angelo.birrer@gmail.com>
2017-12-27 01:57:40 +0100

    Added the base platformer.elm file

--- assets/elm/Platformer.elm
+++ assets/elm/Platformer.elm
@@ -1,7 +1,45 @@
 module Platformer exposing (..)
 
-import Html exposing (..)
+import Html exposing (Html, div, text)
 
-main : Html msg
+-- MAIN
+main : Program Never Model Msg
 main =
-    text "Platformer Game"
+    Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+-- MODEL
+type alias Model =
+    {}
+
+initialModel : Model
+initialModel =
+    {}
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+-- UPDATE
+type Msg
+    = NoOp
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+    div [] [ text "Elm Game Base" ]
